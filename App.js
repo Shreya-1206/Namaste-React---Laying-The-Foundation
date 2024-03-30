b4d62955c655604fa167998745cbd1581aa0ae40
Shreya <shreyaray.1206@gmail.com>
2024-03-30 23:02:25 +0530

    episode 3

---
+++ App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const jsxHeading = <h1>Namaste React ❤️‍🔥</h1>
+// const heading = React.createElement("h1", {id : "title"}, "Namaste React ❤️‍🔥");
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(jsxHeading);
